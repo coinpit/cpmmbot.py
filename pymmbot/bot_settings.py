132ea7bc04de35f4bed077e8014d582aa2c47fcf
Nirmal Gupta <nirmalk.gupta@gmail.com>
2017-08-11 13:36:40 -0700

    added argparse to get key file and urls.

--- pymmbot/bot_settings.py
+++ pymmbot/bot_settings.py
@@ -4,9 +4,6 @@ import logging
 # Available levels: logging.(DEBUG|INFO|WARN|ERROR)
 LOG_LEVEL = logging.INFO
 
-COINPIT_URL = "http://localhost:9000/api/v1"
-BITMEX_URL = "https://testnet.bitmex.com/api/v1/"
-
 COINPIT_SYMBOL = 'BTCUSDW'
 INTEREST_RATE = 0.06
 COINPIT_TICK_SIZE = 1
@@ -19,13 +16,3 @@ BITMEX_TOPICS = ["instrument", "orderBook10", "order", "execution", "position"]
 BITMEX_SYMBOL = 'XBTU17'
 BITMEX_TRAILING_PEG = 10
 HEDGE_INTERVAL = 2  # in seconds
-
-# copy api-key downloaded from coinpit. provide path.
-COINPIT_API_FILE = 'ignore/moVB8e8oWX1oKjaaesdimAGqJHxjih8kjU-c274ee8154f6b7ae.json'
-# save bitmex api key file as json. example
-# {
-#   "ID": "Ag-_DuBvWrO8IK-6FnGhldzR",
-#   "Secret": "_Dss0iITNH5-4QlCNYxqXQ7kA2ngSmhKX4tlRBdn4mmsR3_A"
-# }
-# provide path for the bitmex api key file.
-BITMEX_API_FILE = 'ignore/bitmex_key.json'
--- pymmbot/settings.py
+++ pymmbot/settings.py
@@ -3,18 +3,50 @@ from  easydict import EasyDict as edict
 import pymmbot.bot_settings as baseSettings
 from os import path
 import logging.config
+import argparse
+
+parser = argparse.ArgumentParser(description='Market maker bot for Coinpit with hedging supported at Bitmex')
+parser.add_argument('--bitmex', '-b', dest='bitmex_apikey_file', required=True,
+                    help='Bitmex API KEY json file. example content: \n'
+                         '{'
+                         '  "ID": "Ag-_DuBvWrO8IK-6FnGhldzR",   '
+                         '  "Secret": "_Dss0iITNH5-4QlCNYxqXQ7kA2ngSmhKX4tlRBdn4mmsR3_A" '
+                         '}')
+
+parser.add_argument('--coinpit', '-c', dest='coinpit_apikey_file', required=True,
+                    help='api-key downloaded from coinpit. provide path.')
+
+parser.add_argument('--bh', dest='bitmex_host', help='bitmex host. ex. https://live.coinpit.io/api/v1')
+parser.add_argument('--ch', dest='coinpit_host', help='coinpit host. ex. https://www.bitmex.com/api/v1/')
+args = parser.parse_args()
 
 settings = {}
 settings.update(vars(baseSettings))
 settings = edict(settings)
 
-with open(settings.COINPIT_API_FILE) as API_STRING:
+with open(args.coinpit_apikey_file) as API_STRING:
     settings.COINPIT_API_KEY = json.load(API_STRING)
 
-with open(settings.BITMEX_API_FILE) as API_STRING:
+with open(args.bitmex_apikey_file) as API_STRING:
     as_json = json.load(API_STRING)
     settings.BITMEX_API_KEY = as_json['ID']
     settings.BITMEX_API_SECRET = as_json['Secret']
 
+network = 'livenet' if settings.COINPIT_API_KEY['userid'][0] == '1' else 'testnet'
+
+if args.coinpit_host is not None:
+    settings.COINPIT_URL = args.coinpit_host
+else:
+    settings.COINPIT_URL = {"livenet": "https://live.coinpit.io/api/v1",
+                            "testnet": "https://live.coinpit.me/api/v1"}[network]
+
+if args.bitmex_host is not None:
+    settings.BITMEX_URL = args.bitmex_host
+else:
+    settings.BITMEX_URL = {"livenet": "https://www.bitmex.com/api/v1/",
+                           "testnet": "https://testnet.bitmex.com/api/v1/"}[network]
+
+print("#### bitmex", settings.BITMEX_URL)
+print("#### coinpit", settings.COINPIT_URL)
 log_file_path = path.join(path.dirname(path.abspath(__file__)), 'logging.conf')
 logging.config.fileConfig(log_file_path)
